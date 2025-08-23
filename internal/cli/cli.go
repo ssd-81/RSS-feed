311b5603f63d117a2bf6913806067e1c65105a01
ssd-81 <arcsagar.91@gmail.com>
2025-08-23 13:10:59 +0530

    added user register handler in cli package

--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -1,8 +1,14 @@
 package cli
 
 import (
+	"context"
+	"database/sql"
 	"fmt"
+	"os"
+	"time"
 
+	"github.com/google/uuid"
+	"github.com/ssd-81/RSS-feed-/internal/database"
 	"github.com/ssd-81/RSS-feed-/internal/types"
 )
 
@@ -21,6 +27,34 @@ func HandlerLogin(s *types.State, cmd Command) error {
 	}
 	s.Cfg.UserName = cmd.Arguments[0]
 	fmt.Println("the user has been set successfully")
+	return nil
+}
+
+func HandlerRegister(s *types.State, cmd Command) error {
+	if len(cmd.Arguments) == 0 {
+		return fmt.Errorf("the register command expects a single argument, the username")
+	}
+
+	// checking if the user already exists in the database
+	// and exit prematurely if the user already exists
+	_, err := s.Db.GetUser(context.Background(), cmd.Arguments[1])
+	if err == nil {
+		os.Exit(1)
+	}
+
+	// creating arg for passing to CreateUser function
+	params := database.CreateUserParams{
+		ID:        uuid.NullUUID{UUID: uuid.New(), Valid: true},
+		CreatedAt: sql.NullTime{Time: time.Now(), Valid: true},
+		UpdatedAt: sql.NullTime{Time: time.Now(), Valid: true},
+		Name:      cmd.Arguments[1],
+	}
+	user, err := s.Db.CreateUser(context.Background(), params)
+	if err != nil {
+		return fmt.Errorf("could not create the %s user in the database", params.Name)
+	}
+	fmt.Println("user: ", user, " was successfuly registered in database")
+
 	return nil
 }
 
--- sql/queries/users.sql
+++ sql/queries/users.sql
@@ -9,4 +9,5 @@ VALUES (
 RETURNING *;
 
 -- name: GetUser :one
-SELECT * FROM users WHERE name = $1;
+SELECT * FROM users 
+WHERE name = $1 LIMIT 1;
