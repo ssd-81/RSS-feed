5355406edd204329634876e9c5a9d93a128dbc40
ssd-81 <masterarc@yahoo.com>
2025-09-03 19:01:58 +0530

    update project to current status

---
+++ sql/schema/003_feed_follows.sql
@@ -0,0 +1,12 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE feed_follows(id UUID PRIMARY KEY, created_at timestamp, updated_at timestamp, user_id UUID,feed_id UUID, CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+CONSTRAINT fk_feed FOREIGN KEY (feed_id) REFERENCES feeds(id) ON DELETE CASCADE,
+CONSTRAINT unique_user_feed UNIQUE (user_id, feed_id));
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE feeds_follows·;
+-- +goose StatementEnd
+
